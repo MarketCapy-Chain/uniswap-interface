31727baf19b2f3b4619838ccd91565f36ee62229
MarketCapy <popcatsr@gmail.com>
2025-06-17 03:53:03 +0100

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,8 +5,8 @@ import { Text } from 'rebass'
 
 import styled from 'styled-components'
 
-import Logo from '../../assets/images/nexus-logo.png'
-import LogoDark from '../../assets/images/nexus-logo.png'
+import Logo from '../../assets/images/capy-logo.png'
+import LogoDark from '../../assets/images/capy-logo.png'
 import { useActiveWeb3React } from '../../hooks'
 import { useDarkModeManager } from '../../state/user/hooks'
 import { useETHBalances } from '../../state/wallet/hooks'
